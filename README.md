8af27a72e441567b7849ccd70342bc1cbf6f7b27
ABudnik-mtrx <e.v.budnik@gmail.com>
2021-04-08 23:11:07 +0300

    Second lesson

---
+++ README.md
@@ -0,0 +1,11 @@
+my_list = [123, 6.66, True, 'str', [0,1,2,3,4,5,6]]
+
+print (type(my_list [0]))
+
+print (type(my_list [1]))
+
+print (type(my_list [2]))
+
+print (type(my_list [3]))
+
+print (type(my_list [4]))
---
+++ les 2_ex2.py
@@ -0,0 +1,33 @@
+
+my_list = list(input('Введите значения: '))
+print (my_list)
+
+a = list(my_list[-1])
+
+
+if len(my_list)%2 == 0:
+    my_list[::2], my_list[1::2] = my_list[1::2],my_list[::2]
+    print(my_list)
+else:
+    new_my_list = my_list[:-1]
+    new_my_list[::2], new_my_list[1::2] = new_my_list[1::2], new_my_list[::2]
+    q= new_my_list
+    print (q+a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
